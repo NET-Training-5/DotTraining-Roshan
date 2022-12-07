e72dcc3bce3ff31a24733662dff43342f217c434
Roshan Joshi <roshanjoshi224@gmail.com>
2022-12-07 07:05:29 +0545

    add console project

---
+++ Methods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals1
+{
+    internal class Methods
+    {
+        //simplest possible :  returns nothung, takes no arguments.   
+
+       public void PrintMessage()
+        {
+            Console.WriteLine("Nepal is beautiful");
+        }
+
+        //returns nothing takes some arguments 
+
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        } 
+
+
+        //returns something  takes no arguments
+
+        public string GetMessage()
+        {
+            return "kathmandu";
+        }
+
+        //returns something , takes arguments
+
+        public double Add(double a, double b) => a + b;
+
+
+        // Expression bodied members
+
+       public double Numbers(double a, double b, double c)
+        {
+            double average = (a + b + c) / 2;
+            return average;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Fundamentals1;
+
+byte age = 23;
+string name = "Ram";
+var m = name + "ïs" + age + "years old";
+var n = $"{name}is{age}years old"; // string interpolation.
+
+Console.WriteLine(m);
+Console.WriteLine(n);
+
+
+Methods a = new();
+a.PrintMessage();
+
+a.Print("nepal");
+
+var hello= a.GetMessage();
+Console.WriteLine(hello);
+
+var sum = a.Add(10.23, 20.23);
+Console.WriteLine(sum);
+
+var average = a.Numbers(10, 20, 30);
+Console.WriteLine(average);
+
+//takes argunents or not , returns multiple values
+
+
