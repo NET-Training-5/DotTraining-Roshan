2991575774f42bd9749b1035cebe54efaf221d44
Roshan Joshi <roshanjoshi224@gmail.com>
2022-12-11 08:08:21 +0545

    new commit

--- GetterSetter/Program.cs
+++ GetterSetter/Program.cs
@@ -17,6 +17,22 @@ namespace Pracrice
             Console.WriteLine(SUPA_Girs.studentsCapacity);
             Console.WriteLine(SUPA.Type);
             Console.ReadLine();
+
+
+            Fifa worldcup2018 = new Fifa();
+            {
+            worldcup2018.host = "Russia";
+            worldcup2018.year = new DateTime(2018, 2, 3);
+            worldcup2018.winner = "France";
+            worldcup2018.teams = new string[]{"France","Germany","Morocco","crotia"};
+            Fifa worldcup2020 = new Fifa("Quatar");
+
+    	    Fifa worldcup2022 =  new Fifa("Qatar", new DateTime(2002, 2,2), "Nepal",new string[]
+            {"France","Germany","Morocco","crotia"});
+
+            Fifa.NoOfGroups = 9;
+
+
         }
         }
     }
