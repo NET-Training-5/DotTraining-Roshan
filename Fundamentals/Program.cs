58b56e047b0ec2367189c6aa61d838d354e4a586
Roshan Joshi <roshanjoshi224@gmail.com>
2022-12-12 10:19:47 +0545

    file transferred

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Fundamentals1;
+
+internal class NewBaseType
+{
+    private static void Main(string[] args)
+    {
+        byte age = 23;
+        string name = "Ram";
+        var m = name + "ïs" + age + "years old";
+        var n = $"{name}is{age}years old"; // string interpolation.
+
+        Console.WriteLine(m);
+        Console.WriteLine(n);
+
+
+        Methods a = new();
+        a.PrintMessage();
+
+        a.Print("nepal");
+
+        var hello = a.GetMessage();
+        Console.WriteLine(hello);
+
+        var sum = a.Add(10.23, 20.23);
+        Console.WriteLine(sum);
+
+        var average = a.Numbers(10, 20, 30);
+        object value = Console.WriteLine(average);
+
+        string[] value1 = { "Ram", "shyam", "hari" };
+
+        a.FindMinMax();
+
+
+
+
+        Drawing.Gen = new();
+        Gen.DrawPattern();
+        Gen.DrawPattern2();
+
+        var (min, max) = Gen.FindMinMax(2, 453, 23, 53, 1, 24, 1, 3434, 21, 2);
+        System.Console.WriteLine( $"Min:{min} Max:{max}");
+
+    }
+}
+
+
+
+//takes argunents or not , returns multiple values
+
+
