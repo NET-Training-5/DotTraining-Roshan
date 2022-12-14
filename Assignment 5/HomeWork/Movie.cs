9c8e29b7095c7e35f90a6fdb764ff696f612cbee
Roshan Joshi <roshanjoshi224@gmail.com>
2022-12-14 20:35:36 +0545

    Assignment5

---
+++ Assignment 5/HomeWork/Movie.cs
@@ -0,0 +1,32 @@
+interface movieKoBau
+{
+
+    string movieName;
+    string movieType;
+
+
+
+    public string GetmovieName()
+    {
+
+    }
+
+    public string GetmovieType()
+    {
+
+
+    }
+
+
+
+
+
+}
+class AnimeMovie
+{
+
+    public string movieType{get;set;}
+    public string movieName{get; set;}
+
+}
+
---
+++ Assignment 5/HomeWork/main.cs
@@ -0,0 +1,10 @@
+using System;
+
+static void Main(string[]args){
+    AnimeMovie am = new();
+    var s = am.movieName(Roshan);
+    var d = am.movieType(anime);
+    System.Console.WriteLine(s);
+    System.Console.WriteLine(d);
+
+}
