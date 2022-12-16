e030e6a4594f63e59b4e28782d3792f072c6baa3
Roshan Joshi <roshanjoshi224@gmail.com>
2022-12-16 06:55:51 +0545

    upadted.

--- FileHandling/LINQ/Linq.cs
+++ FileHandling/LINQ/Linq.cs
@@ -5,22 +5,22 @@ class Linq
 {
     int[] numnber = {12,32,213,65,790,9,643,2,56,8709,432,4,570,90,79,40};
 
-    void LearnToQuery()
+   public void LearnToQuery()
         {
             //finding even number in array
             var evennumber = numnber.Where(x=> x%2 == 0);
 
             //finding odd number
-            var oddnumber = numnber.Where(x=> x %2  != 0);
+            var oddnumber = numnber.Where(x=> x %2  != 0); 
 
 
             //List multiples of 5 and 7 from "numbers"
 
-            var muntipleOF = numnber.Select(x => x%5==0 && x%7==0);
+            var muntipleOF = numnber.Where(x => x%5==0 && x%7==0);
 
             //List all items less than 100 and ends with 0, from "numbers"
 
-            var item = numnber.Select(x=> x%10==0);
+            var item = numnber.Where(x=> x%10==0);
 
         }
 
