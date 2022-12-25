ba5c3816e5f67a4c8eaf8b148c4e06c7935d2302
Roshan Joshi <roshanjoshi224@gmail.com>
2022-12-25 15:24:11 +0545

    Learning_MVC

---
+++ HelloWeb/HelloWeb/Controllers/NumberController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWeb.Controllers
+{
+    public class NumberController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Greet()
+        {
+            return View();
+        }
+    }
+}
